8ec7bc8d06fa00dd3dd0c3869e14175356c7d309
valfink <117605562+valfink@users.noreply.github.com>
2023-03-07 11:52:14 +0100

    Add dummy login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Link, Route, Routes} from "react-router-dom";
 import SignUp from "./component/SignUp";
+import Login from "./component/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Link to={"/sign-up"}>Sign up</Link><br/>
       <Routes>
         <Route path={"/sign-up"} element={<SignUp />} />
+        <Route path={"/login"} element={<Login />} />
       </Routes>
     </div>
   );
